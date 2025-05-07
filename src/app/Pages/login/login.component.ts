5db9c8e9d0f6a49a38976185f7db4f841aa9d99c
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-05-07 13:06:55 -0500

    ✨ feat: LoginForgot
    Se agrega el forgot password, funcional visualmente, falta el BackEnd, tambien corrigiendo problemas con el login html y el ts, ya que el diseño estaba mal acomodado con el navbar

--- src/app/Pages/login/login.component.ts
+++ src/app/Pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';  // Importamos el Router para redirigir al usuario
+import { RouterModule, Router } from '@angular/router';  
 import { AuthService } from '../../Shared/services/auth.service';
 import { User } from '../../models/user.model';
 import { CommonModule } from '@angular/common';
@@ -8,7 +8,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterModule],
   templateUrl: './login.component.html',
   
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from '../app/Pages/home/home.component';
 import { TxtInputComponent } from '../app/Shared/components/txt-input/txt-input.component';
 import { ChatMessageComponent } from '../app/Shared/components/chat-message/chat-message.component';
 import { LoginComponent } from '../app/Pages/login/login.component';
+import { ForgotPasswordComponent } from '../app/Pages/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección por defecto a Home
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirección por defecto a Home
     { path: 'home', component: HomeComponent },
     { path: 'input', component: TxtInputComponent },
     { path: 'chat', component: ChatMessageComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
     { path: '**', redirectTo: 'home' }, // Ruta comodín para rutas no válidas
 ];
