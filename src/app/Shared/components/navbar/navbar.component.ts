3d0f4e5eb93ac2a8afcc3dd417fecb5fc943d62b
Varelaso32 <samyst2013@hotmail.com>
2025-05-07 01:53:40 -0500

    ✨ feat: Add Home
    Se agrega el home

--- src/app/Shared/components/navbar/navbar.component.ts
+++ src/app/Shared/components/navbar/navbar.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  @ViewChild('searchBox') searchBox!: ElementRef;
+  @ViewChild('searchContainer') searchContainer!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  // Función para mostrar u ocultar el campo de búsqueda
+  toggleSearch() {
+    const searchElement = this.searchBox.nativeElement;
+    const searchButton =
+      this.searchContainer.nativeElement.querySelector('.search-btn');
+
+    // Si el campo de búsqueda está oculto, lo mostramos
+    if (searchElement.classList.contains('hidden')) {
+      this.renderer.removeClass(searchElement, 'hidden');
+      this.renderer.addClass(searchButton, 'ml-12'); // Mover el botón y el input juntos
+      this.renderer.addClass(searchElement, 'w-64'); // Aumentar el ancho del input
+      this.renderer.addClass(searchButton, 'mr-2'); // Agregar un margen al botón
+    } else {
+      // Si ya está visible, lo ocultamos
+      this.renderer.addClass(searchElement, 'hidden');
+      this.renderer.removeClass(searchButton, 'ml-12'); // Mover el botón de vuelta
+      this.renderer.removeClass(searchElement, 'w-64'); // Reducir el ancho del input
+      this.renderer.removeClass(searchButton, 'mr-2'); // Eliminar el margen extra
+    }
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from '../app/Pages/home/home.component';
 import { TxtInputComponent } from '../app/Shared/components/txt-input/txt-input.component';
-import { BtnButtonComponent } from '../app/Shared/components/btn-button/btn-button.component';
 import { ChatMessageComponent } from '../app/Shared/components/chat-message/chat-message.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección por defecto a Home
     { path: 'home', component: HomeComponent },
     { path: 'input', component: TxtInputComponent },
-    { path: 'button', component: BtnButtonComponent },
     { path: 'chat', component: ChatMessageComponent },
     { path: '**', redirectTo: 'home' }, // Ruta comodín para rutas no válidas
 ];
