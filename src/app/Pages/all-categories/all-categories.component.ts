2c3d1ce62bcb248ec557c1bd974b120919a5d669
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2025-05-13 23:44:46 -0500

    🐞 fix: Carrito
    Se arreglan errores, se agrega boton para agregar al carrito

--- src/app/Pages/all-categories/all-categories.component.ts
+++ src/app/Pages/all-categories/all-categories.component.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from '../../Shared/components/navbar/navbar.component
 import { FooterComponent } from '../../Shared/components/footer/footer.component';
 import { RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
+import { CartService } from '../../Shared/services/cart.service';
 
 @Component({
   selector: 'app-all-categories',
@@ -17,6 +18,7 @@ export class AllCategoriesComponent {
   groupedProducts$: Observable<Record<string, Product[]>>;
   private productService = inject(ProductService);
   private location = inject(Location);
+  private cartService = inject(CartService);
 
   constructor() {
     this.groupedProducts$ = this.productService.getProducts().pipe(
@@ -32,5 +34,8 @@ export class AllCategoriesComponent {
 
   goBack(): void {
     this.location.back();
+  }
+  addToCart(product: Product): void {
+    this.cartService.addToCart(product); 
   }
 }
