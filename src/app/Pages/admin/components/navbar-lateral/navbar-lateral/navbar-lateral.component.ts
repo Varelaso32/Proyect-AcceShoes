a78cdffb434c6f4ea3347f40417439180e273167
Varelaso32 <samyst2013@hotmail.com>
2025-05-22 22:04:12 -0500

    ✨ feat: Add navbar

--- src/app/Pages/admin/components/navbar-lateral/navbar-lateral/navbar-lateral.component.ts
+++ src/app/Pages/admin/components/navbar-lateral/navbar-lateral/navbar-lateral.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar-lateral',
   standalone: true,
-  imports: [RouterLink, RouterModule],
+  imports: [CommonModule, RouterLink, RouterModule],
   templateUrl: './navbar-lateral.component.html',
   styleUrl: './navbar-lateral.component.css',
 })
 export class NavbarLateralComponent {
+  submenuStates: { [key: string]: boolean } = {};
+  profileMenuOpen = false;
 
+  toggleSubmenu(name: string) {
+    this.submenuStates[name] = !this.submenuStates[name];
+  }
+
+  isSubmenuOpen(name: string): boolean {
+    return !!this.submenuStates[name];
+  }
+
+  toggleProfileMenu() {
+    this.profileMenuOpen = !this.profileMenuOpen;
+  }
+
+  // Para cerrar el menú si se hace clic fuera
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.profile-dropdown-container')) {
+      this.profileMenuOpen = false;
+    }
+  }
 }
---
+++ src/app/Pages/admin/gestion-categorias/gestion-categorias.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-categorias',
+  imports: [],
+  templateUrl: './gestion-categorias.component.html',
+  styleUrl: './gestion-categorias.component.css'
+})
+export class GestionCategoriasComponent {
+
+}
