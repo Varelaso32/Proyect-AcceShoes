931ff3442c85334d957a97f5c53fa782723419eb
Varelaso32 <samyst2013@hotmail.com>
2025-05-23 01:04:50 -0500

    ✨ feat: Add GestionPlanes

---
+++ src/app/Pages/admin/revisiones/gestion-pqrsd/gestion-pqrsd.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-gestion-pqrsd',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './gestion-pqrsd.component.html',
+  styleUrl: './gestion-pqrsd.component.css',
+})
+export class GestionPqrsdComponent {}
